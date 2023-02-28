147f462bdb8d4f5651a37359ee75c81544cb8246
Rafael Zardo <rafael.zardo@outlook.com>
2023-02-28 11:36:41 -0300

    add exercicio 5

---
+++ exercicio5.js
@@ -0,0 +1,8 @@
+let stringInicial = "InvertendoIsso"
+let stringInvertida = "";
+
+for (let i = stringInicial.length - 1; i >= 0; i--) stringInvertida += stringInicial[i];
+
+console.log("String Inicial: " + stringInicial);
+console.log("String Invertida: " + stringInvertida);
+
